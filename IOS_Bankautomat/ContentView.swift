e869360047f4ad95ca9eba51905086a125b081d5
Stefan Jessel <stefanjessel@MacBook-Pro-von-Stefan.local>
2022-06-16 13:17:17 +0200

    Init Commit

---
+++ IOS_Bankautomat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS_Bankautomat
+//
+//  Created by Stefan Jessel on 16.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS_Bankautomat/IOS_BankautomatApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_BankautomatApp.swift
+//  IOS_Bankautomat
+//
+//  Created by Stefan Jessel on 16.06.22.
+//
+
+import SwiftUI
+
+@main
+struct IOS_BankautomatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
